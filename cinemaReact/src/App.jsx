18449da46b8181ff67e6e04d7e3f664602e621f7
Bong127 <ansqhdgus200@naver.com>
2025-01-16 10:25:05 +0900

    2025-01-16

--- cinemaReact/src/App.jsx
+++ cinemaReact/src/App.jsx
@@ -14,6 +14,7 @@ import Layout from './components/Layout/Layout'
 import MovieChart from './pages/movie/movieChart/MovieChart'
 import MovieSearch from './pages/movie/Search/MovieSearch'
 import MovieInfo from './pages/movie/MovieInfo/MovieInfo'
+import Ticket from './pages/Ticket'
 function App() {
 
   return (
