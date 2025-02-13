518a53bc9f8c16aa28f6763464f6ae977040b768
Bong127 <ansqhdgus200@naver.com>
2025-02-13 18:18:50 +0900

    영화 정보 페이지 수정

--- App/cinema_flutter/lib/service/movie_service.dart
+++ App/cinema_flutter/lib/service/movie_service.dart
@@ -6,7 +6,7 @@ class MovieService {
   final Dio dio = Dio();
   final String host = 'http://10.0.2.2:8080';
 
-  // 데이터 목록 조회
+  // 홈 화면 조회
   Future<Map<String, dynamic>> list() async{
     var url = "$host/";
     dynamic list;
@@ -19,8 +19,21 @@ class MovieService {
     return list;
   }
 
-  // 데이터 목록 조회
-  Future<Map<String, dynamic>> select(String id) async{
+  // 리뷰리스트 조회
+  Future<Map<String, dynamic>> reviewList(String id, String username) async{
+    var url = "$host/review/list?id=$id&username=$username";
+    dynamic list;
+    try{
+      Response response = await dio.get(url);
+      return response.data as Map<String, dynamic>;
+    }catch(e){
+      print(e);
+    }
+    return list;
+  }
+
+  // 영화정보 조회
+  Future<Map<String, dynamic>> movieInfo(String id) async{
     var url = "$host/movie/movieInfo?id=$id";
     dynamic list;
     try{
@@ -33,20 +46,28 @@ class MovieService {
   }
 
   // 프로필 이미지 불러오기 
-  Future<String> getUser(String userId) async {
-  var url = "$host/usersss/profile?id=$userId"; // 사용자 정보 가져오는 URL
-  try {
-    // 사용자 정보 요청
-    Response response = await dio.get(url);
-    var responseData = response.data as Map<String, dynamic>;
-
-    // orifile의 id를 추출
-    String orifileId = responseData['orifile']['id'];
-
-    return orifileId;  // 사용자 정보와 이미지 데이터를 포함한 Map 반환
-  } catch (e) {
-    print(e);
-    return "";
-  }
+  Future<String?> getUser(String userId) async {
+    var url = "$host/usersss/profile?id=$userId"; // 사용자 정보 가져오는 URL
+    try {
+      Response response = await dio.get(url);
+      if (response.statusCode == 200) {
+        var responseData = response.data;
+        if (responseData == null)
+          return null;
+        if (responseData is! Map<String, dynamic>)
+          return null;
+        if (!responseData.containsKey("orifile") || responseData["orifile"] == null)
+          return null;
+        var orifile = responseData["orifile"];
+        if (orifile is! Map<String, dynamic> || !orifile.containsKey("id"))
+          return null;
+        String orifileId = orifile["id"].toString();
+        return orifileId;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      return null;
+    }
 }
 }
