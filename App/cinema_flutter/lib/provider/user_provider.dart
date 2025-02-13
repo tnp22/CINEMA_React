e9b592a98f606122c83d74ec73fe6114c8076c77
1015jw <971015jw@gmail.com>
2025-02-13 09:46:16 +0900

    202502132

--- App/cinema_flutter/lib/provider/user_provider.dart
+++ App/cinema_flutter/lib/provider/user_provider.dart
@@ -1,4 +1,3 @@
-
 import 'package:cinema_flutter/model/users.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -6,14 +5,13 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider extends ChangeNotifier {
-
   // 상태 관리할 정보
   // 사용자 정보 👩‍💼
   late Users _userInfo;
   // 로그인 상태 🔐
   bool _loginStat = false;
 
-  int selectedIndex=0;
+  int selectedIndex = 0;
 
   // getter
   Users get userInfo => _userInfo;
@@ -34,20 +32,19 @@ class UserProvider extends ChangeNotifier {
   final storage = const FlutterSecureStorage();
 
   /// 🔐 로그인 요청
-  Future<void> login(
-      String username, String password, 
-      {bool rememberId=false, bool rememberMe=false}
-    ) async {
+  Future<void> login(String username, String password,
+      {bool rememberId = false, bool rememberMe = false}) async {
     _loginStat = false; // 로그인 여부 초기화
-    
-    String url = 'http://10.0.2.2:8080/login?username=$username&password=$password';
+
+    String url =
+        'http://10.0.2.2:8080/login?username=$username&password=$password';
 
     try {
       final response = await _dio.get(url);
-      if( response.statusCode == 200 ) {
+      if (response.statusCode == 200) {
         // JWT ➡ 🎁 SecureStorage 에 저장
         final authorization = response.headers['authorization']?.first;
-        if( authorization == null ) {
+        if (authorization == null) {
           print("아이디 또는 비밀번호가 일치하지 않습니다.");
           return;
         }
@@ -58,35 +55,31 @@ class UserProvider extends ChangeNotifier {
         await storage.write(key: 'jwt', value: jwt);
 
         // 사용자 정보, 로그인 상태 ➡ ⚡ Provider 에 업데이트
-        _userInfo = Users.fromMap( response.data );
+        _userInfo = Users.fromMap(response.data);
         _loginStat = true;
         // ########## 로그인 처리 ##########
 
         // 아이디 저장
-        if( rememberId ) {
+        if (rememberId) {
           print("아이디 저장");
           await storage.write(key: 'username', value: username);
-        }
-        else {
+        } else {
           print("아이디 저장 해제");
           await storage.delete(key: 'username');
         }
 
         // 자동 로그인
-        if( rememberMe ) {
+        if (rememberMe) {
           print("자동 로그인");
           final prefs = await SharedPreferences.getInstance();
           await prefs.setBool('auto_login', true);
-        } 
-        else {
+        } else {
           final prefs = await SharedPreferences.getInstance();
           await prefs.setBool('auto_login', false);
         }
-      }
-      else if( response.statusCode == 403 ) {
+      } else if (response.statusCode == 403) {
         print('아이디 또는 비밀번호가 일치하지 않습니다.');
-      }
-      else {
+      } else {
         print('네트워크 오류 또는 알 수 없는 오류로 로그인에 실패하였습니다.');
       }
     } catch (e) {
@@ -96,7 +89,6 @@ class UserProvider extends ChangeNotifier {
     // 업데이트 된 상태를 구독하고 있는 위젯에 다시 빌드
     notifyListeners();
   }
-  
 
   // 사용자 정보 요청
   Future<bool> getUserInfo() async {
@@ -105,24 +97,20 @@ class UserProvider extends ChangeNotifier {
       String? jwt = await storage.read(key: 'jwt');
       print('jwt : $jwt');
 
-      final response = await _dio.get(
-                                      url,
-                                      options: Options(
-                                        headers: {
-                                          'Authorization' : 'Bearer $jwt',
-                                          'Content-Type' : 'application/json'
-                                        }
-                                      )
-                                    );
-      if( response.statusCode == 200 ) {
+      final response = await _dio.get(url,
+          options: Options(headers: {
+            'Authorization': 'Bearer $jwt',
+            'Content-Type': 'application/json'
+          }));
+      if (response.statusCode == 200) {
         final userInfo = response.data;
         print('userInfo : $userInfo');
-        if( userInfo == null ) {
+        if (userInfo == null) {
           return false;
         }
         // provider 에 사용자 정보 저장
         _userInfo = Users.fromMap(userInfo);
-        notifyListeners();  // 구독 하고 있은 위젯에 공유
+        notifyListeners(); // 구독 하고 있은 위젯에 공유
         return true;
       } else {
         print("사용자 정보 조회 실패");
@@ -132,23 +120,48 @@ class UserProvider extends ChangeNotifier {
       print('사용자 정보 요청 실패 : $e');
       return false;
     }
-
   }
 
+  // 사용자 정보 업데이트 메서드
+  Future<void> updateUserInfo(String email, String password) async {
+    final url = 'http://10.0.2.2:8080/users/update';
+    try {
+      String? jwt = await storage.read(key: 'jwt');
+      final response = await _dio.post(url,
+          data: {
+            'email': email,
+            'password': password,
+          },
+          options: Options(headers: {
+            'Authorization': 'Bearer $jwt',
+            'Content-Type': 'application/json'
+          }));
+      if (response.statusCode == 200) {
+        // 사용자 정보 업데이트 성공 시, provider에 저장
+        _userInfo.email = email;
+        _userInfo.password = password;
+        notifyListeners();
+      } else {
+        print("사용자 정보 업데이트 실패");
+      }
+    } catch (e) {
+      print('사용자 정보 업데이트 실패 : $e');
+    }
+  }
 
   // 자동 로그인 처리
   Future<void> autoLogin() async {
     final prefs = await SharedPreferences.getInstance();
     final rememberMe = prefs.getBool('auto_login') ?? false;
     print('자동 로그인');
-    if( rememberMe ) {
+    if (rememberMe) {
       final jwt = await storage.read(key: 'jwt');
-      if( jwt != null ) {
+      if (jwt != null) {
         // 사용자 정보 요청
-        bool result =  await getUserInfo();
+        bool result = await getUserInfo();
 
         // 사용자 요청 정보 응답 성공 시, 로그인 여부 true 로 설정
-        if( result ) {
+        if (result) {
           _loginStat = true;
           notifyListeners();
         }
@@ -176,5 +189,4 @@ class UserProvider extends ChangeNotifier {
     }
     notifyListeners();
   }
-
 }
